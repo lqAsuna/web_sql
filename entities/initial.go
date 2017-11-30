8b1e1e8a1393825586e46e6e0f75484686a8f6d6
lqAsuna <32841893+lqAsuna@users.noreply.github.com>
2017-11-30 17:15:07 +0800

    Update initial.go

--- entities/initial.go
+++ entities/initial.go
@@ -1,1 +1,23 @@
+package entities
 
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+)
+
+var engine *xorm.Engine
+
+func init() {
+	// Init xorm engine
+	Engine, err := xorm.NewEngine("mysql", "root:root@tcp(127.0.0.1:3306)/test?charset=utf8&parseTime=true")
+	checkErr(err)
+	err = Engine.Sync2(new(UserInfo))
+	checkErr(err)
+	engine = Engine
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
